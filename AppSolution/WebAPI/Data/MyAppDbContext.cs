37ffa0e40fff0cb1267170c9cc8340643d529748
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-09-17 20:34:34 +0800

    initial commit

---
+++ AppSolution/WebAPI/Data/MyAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Db01;Integrated Security=True");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
